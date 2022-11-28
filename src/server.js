e653ecded03fb1914c8d1f5cc66174b25d3bef71
nedelkovskatamara <nedelkovska.tamara@gmail.com>
2022-11-28 00:48:41 -0800

    Configuring app for deployment on heroku

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(__dirname + '/dist/battlefy-league-of-legends-app'));
+
+app.get('/*', function(req,res) {
+    res.sendFile(path.join(__dirname+'/dist/battlefy-league-of-legends-app/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
